9a87e11717f12d4b2ec38af099011ae857ebc37a
Ashot Aghajanyan <ashot.aghajanyan@mauriceward.com>
2020-12-14 11:01:57 +0400

    2 more commands added

--- src/globalVariables.ts
+++ src/globalVariables.ts
@@ -6,4 +6,6 @@ export const infoCards = [
     { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
     { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
     { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+    { color: '#7c3482', title: 'Go back to main page', text: 'Go back' },
+    { color: '#546e7a', title: 'Open a specific article from the list', text: 'Open the article number <number>' },
   ];
